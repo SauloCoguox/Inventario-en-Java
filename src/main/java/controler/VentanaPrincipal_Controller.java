f7f81fbce6722fdf72f396c26da500200de043e9
SauloCoguox <jconergt@gmail.com>
2024-10-16 21:43:45 -0600

    Primera vercion

---
+++ src/main/java/controler/VentanaPrincipal_Controller.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controler;
+
+import ventana.Ventana_Principal;
+
+/**
+ *
+ * @author coguox
+ */
+public class VentanaPrincipal_Controller {
+    
+    //Cremos objetos
+    Ventana_Principal view;
+    
+    //Creacion de metodo para poder visualizar los objetos
+    public VentanaPrincipal_Controller (Ventana_Principal view){
+    
+        this.view = view;
+    }
+    
+}
